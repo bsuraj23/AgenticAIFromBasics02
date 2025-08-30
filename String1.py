a9d79a598fcce6bd67feb0b8bef1d96ff042d31e
Sindhu_Togaru <sindhutogaru02@gmail.com>
2025-08-30 14:25:44 +0530

    adding code

---
+++ String1.py
@@ -0,0 +1,8 @@
+# Python3 program to show the
+# working of upper() function
+text = 'geeKs For geEkS'
+
+# upper() function to convert
+# string to upper case
+print("\nConverted String:")
+print(text.upper())
