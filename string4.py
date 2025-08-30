56d566a6a9235e39ecfc03753fae10e0d3ad7e7c
Sindhu_Togaru <sindhutogaru02@gmail.com>
2025-08-30 14:52:15 +0530

    adding swapcase() code

---
+++ string4.py
@@ -0,0 +1,7 @@
+# swaps the case of all characters in the string
+# upper case character to lowercase and viceversa
+
+text = 'geeKs For geEkS'
+
+print("\nConverted String:")
+print(text.swapcase())
