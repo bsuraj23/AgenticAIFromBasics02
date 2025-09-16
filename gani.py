b437de7160e0bea398cc28802abaccb989d9fbfd
Sivareddy <peramsivareddy001@gmail.com>
2025-09-16 12:32:41 +0530

    added fibonacci program

--- gani.py
+++ gani.py
@@ -1,2 +1,10 @@
-lksbdalbhiuljdnkj;ncjkjdndfejuroeri
-sjdjnewdnqw
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+# Example: first 10 Fibonacci numbers
+print(fibonacci(10))
